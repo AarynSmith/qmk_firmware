f9ea453300e90b84926291d094be2a90df5ced11
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2021-03-09 09:36:26 -0600

    update alt layout

--- keyboards/handwired/ortho4x12alt/config.h
+++ keyboards/handwired/ortho4x12alt/config.h
@@ -26,14 +26,14 @@
 #define PRODUCT         4x12alt
 
 /* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 8
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
 
 /* pro_micro pin-out */
-//                         2   3   4   5   6   7
-#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7, E6 }
-//                        RXI, A3, A2, A1, A0, 15, 14, 16
-#define MATRIX_COL_PINS {  D2, F4, F5, F6, F7, B1, B3, B2}
+//                        RXI  2   3   4   6   7
+#define MATRIX_COL_PINS { D2, D1, D0, D4, D7, E6 }
+//                        A3, A2, A1, A0, 15, 14, 16, 10
+#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3, B2, B6}
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
--- keyboards/handwired/ortho4x12alt/keymaps/aarynsmith/config.h
+++ keyboards/handwired/ortho4x12alt/keymaps/aarynsmith/config.h
@@ -14,8 +14,8 @@
 // #define MATRIX_ROW_PINS { D2, D1, D0, D4 }
 //                         5   6   7   8  10  16  14  15  A0  A1  A2  A3
 // #define MATRIX_COL_PINS { C6, D7, E6, B4, B6, B2, B3, B1, F7, F6, F5, F4 }
-#define AUDIO_PIN B5
-#define AUDIO_PIN_ALT B6
+#define AUDIO_PIN C6
+#define AUDIO_PIN_ALT B5
 #define RGB_DI_PIN D3
 
 #define RGBLED_NUM 10
