dac1dea9a68f5e79625a822f0274c77f459bba2e
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2021-03-09 09:02:24 -0600

    update custom layout

--- keyboards/handwired/ortho4x12/keymaps/aarynsmith/keymap.c
+++ keyboards/handwired/ortho4x12/keymaps/aarynsmith/keymap.c
@@ -25,7 +25,7 @@
 // entirely and just use numbers.
 
 // Layer Numbers
-enum layers { _QWERTY = 0, _LOWER, _RAISE, _ADJUST, _SPACEFN, _QMK };
+enum layers { _TEST = 0, _QWERTY, _LOWER, _RAISE, _ADJUST, _SPACEFN, _QMK };
 
 // Layer Keys
 #define LOWER MO(_LOWER)
@@ -44,6 +44,12 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_TEST] = LAYOUT_ortho_4x12(
+    KC_A,    QMKBEST, TBFLIP,  RGB_MOD, RGB_VAI, RGB_VAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+),
 /* Qwerty
  * +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
  * |   tab  |    Q   |    W   |    E   |    R   |    T   |    Y   |    U   |    I   |    O   |    P   |  bksp  |
@@ -56,10 +62,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-HypEsc,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RsEnt,
-QMK,     KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SpFun,  SpFun,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    HypEsc,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RsEnt,
+    QMK,     KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SpFun,  SpFun,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
